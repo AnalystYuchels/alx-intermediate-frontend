fcf79014d4311e91254a203d894e5d2512df8cf6
AnalystYuchels <uche4xploit@gmail.com>
2025-07-12 16:02:50 +0100

    Add isDirector type predicate and executeWork function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -60,3 +60,22 @@ function createEmployee(salary: number | string): Teacher | Director {
 console.log(createEmployee(200));   // Teacher
 console.log(createEmployee(1000));  // Director
 console.log(createEmployee('$500'));// Director
+
+// ... (existing interfaces and classes)
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Test the functions
+
+console.log(executeWork(createEmployee(200)));   // Getting to work
+console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
