1412136b339e2d9e2a6f2382239b7525da5a6b5f
AnalystYuchels <uche4xploit@gmail.com>
2025-07-12 15:42:14 +0100

    Add StudentClass with interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -28,14 +28,50 @@ const director1: Directors = {
 console.log(director1);
 
 // Define the interface for the function
+
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
 // Implement the function
+
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
 // Example usage
+
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
+
+// Constructor interface
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Class interface
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementation
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+
+const student = new StudentClass('Chidera', 'Okonkwo');
+console.log(student.displayName());     // Output: Chidera
+console.log(student.workOnHomework()); // Output: Currently working
